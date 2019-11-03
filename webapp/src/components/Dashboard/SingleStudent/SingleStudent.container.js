50cb90f62f251897d3f928144aaaa0e0d90178ab
Tyler Lafayette <tylerlafayette@gmail.com>
2019-11-03 13:50:39 -0800

    Add Student page

---
+++ webapp/src/components/Dashboard/SingleStudent/SingleStudent.container.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react"
+import { withRouter } from "react-router-dom"
+
+import SingleStudent from "./SingleStudent.component"
+
+export default withRouter(SingleStudent)
